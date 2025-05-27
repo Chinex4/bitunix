036c00ee513400ebda9b554510156c0e9d85f611
ErDprogrammer <rickemzy5@gmail.com>
2025-05-27 10:17:49 +0100

    new push

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://192.168.8.184/cashtradeproApi/api/task/',
+  // baseURL: 'http://192.168.27.134/backend/api/task/',
   withCredentials: true,
 });
 
